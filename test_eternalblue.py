bbc2eefb12edff88c680c208e5d1a9da7893960a
rhaymisonbetini <rhaymison@hotmail.com>
2024-07-03 18:02:59 -0300

    att

--- test_eternalblue.py
+++ test_eternalblue.py
@@ -7,7 +7,7 @@ warnings.filterwarnings('ignore')
 def test_eternalblue():
     hg_token = ''
     language = 'portuguese'
-    audio_path = '/home/deathpirate/Downloads/tester.wav'
+    audio_path = '/home/deathpirate/Downloads/2.wav'
 
     eternal_blue = EternalBlue(hg_token, language)
     teste = eternal_blue.diarize(audio_path)
@@ -15,3 +15,4 @@ def test_eternalblue():
 
 if __name__ == '__main__':
     test_eternalblue()
+
